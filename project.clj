82d73183164495c6b8a2c8a1cc55e4f989843a19
Simon Lomax <simax.99.99@gmail.com>
2014-04-05 16:07:39 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject webdev "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+                :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                           [ring "1.2.2"]
+                           [compojure "1.1.6"]]
+
+  :min-lein-version "2.0.0"
+
+  :uberjar-name "webdev.jar"
+
+  :main webdev.core
+
+  :profiles {:dev
+                 {:main webdev.core/-dev-main}})
+
+
