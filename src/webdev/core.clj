5d5828e480a49559255535eb190e66cb07603390
Simon Lomax <simax.99.99@gmail.com>
2014-04-08 11:26:13 +0100

    Amended main method signature to accept multiple args

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -20,7 +20,7 @@
   (GET "/goodbye" [] goodbye)
   (not-found "Page not found."))
 
-(defn -main [port]
+(defn -main [& [port]]
   (jetty/run-jetty #'app                             {:port (Integer. port)}))
 
 (defn -dev-main [port]
