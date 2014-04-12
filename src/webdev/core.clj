b94b4dad59af5c9e73e348ba718b2752bd65aed7
Simon Lomax <simax.99.99@gmail.com>
2014-04-12 16:00:21 +0100

    Added calc handler

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -20,16 +20,42 @@
    :body "Goodbye, cruel World!"
    :headers {}})
 
+(defn say-my-name [req]
+  {:status 200
+   :body (str "Yo, " (get-in req [:route-params :name]))
+   :headers {}})
+
+(def ops
+  {"+" +
+   "-" -
+   "*" *
+   ":" /})
+
+(defn calc [req]
+  (let [a  (Integer. (get-in req [:route-params :a]))
+        b  (Integer. (get-in req [:route-params :b]))
+        op (get-in req [:route-params :op])
+        f  (get ops op)]
+    (if f
+      {:status 200
+       :body (str (f a b))
+       :headers {}}
+      {:status 404
+       :body "Unkown operator."
+       :headers {}})))
+
 
 (defroutes app
   (GET "/" [] greet)
+  (GET "/yo/:name" [] say-my-name)
+  (GET "/calc/:a/:op/:b" [] calc)
   (GET "/about" [] about)
   (GET "/request" [] handle-dump)
   (GET "/goodbye" [] goodbye)
   (not-found "Page not found."))
 
 (defn -main [& arg]
-  (jetty/run-jetty #'app                             {:port (Integer/parseInt (System/getenv "PORT"))}))
+  (jetty/run-jetty #'app                     {:port (Integer/parseInt (System/getenv "PORT"))}))
 
 (defn -dev-main [port]
   (jetty/run-jetty (wrap-reload #'app)       {:port (Integer. port)}))
